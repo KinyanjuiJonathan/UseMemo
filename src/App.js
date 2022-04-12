59efdb1f19bcb7a2353f1b9f2282d6fbb880dd16
KinyanjuiJonathan <83952856+KinyanjuiJonathan@users.noreply.github.com>
2022-04-12 23:31:58 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      import { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+    </div>
+  );
+}
+
+export default App;
